6cd061658c46a00f38b42415fb5f4e110b1305f2
Oleksii Borshch <sergeevi44@gmail.com>
2022-05-04 19:23:02 +0300

    task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+	body: document.querySelector("body"),
+	startBtn: document.querySelector("[data-start]"),
+	stopBtn: document.querySelector("[data-stop]"),
+};
 
+refs.startBtn.addEventListener("click", onStartBtnClick);
+refs.stopBtn.addEventListener("click", onStopBtnClick);
+
+function onStartBtnClick() {
+	refs.startBtn.disabled = true;
+	return (changeColor = setInterval(() => {
+		refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+	}, 1000));
+}
+
+function onStopBtnClick() {
+	refs.startBtn.disabled = false;
+	clearInterval(changeColor);
+}
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
