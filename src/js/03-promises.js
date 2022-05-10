15d36c9194a8219fb4970185db84f7d8c1bd3d2b
Oleksii Borshch <sergeevi44@gmail.com>
2022-05-10 19:17:37 +0300

    start task3 on new branch

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import Notiflix from "notiflix";
+
+const refs = {
+	submitBtn: document.querySelector('button[type="submit"]'),
+	form: document.querySelector(".form"),
+};
+
+refs.submitBtn.addEventListener("click", onSubmitBtnClick);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+	const shouldResolve = Math.random() > 0.3;
+	if (shouldResolve) {
+		resolve(); // Fulfill
+	} else {
+		// Reject
+	}
 }
+
+function onSubmitBtnClick(evt) {
+	evt.preventDefault();
+	const delay = refs.form.delay.value;
+	const step = refs.form.step.value;
+	const amount = refs.form.amount.value;
+	console.log({ delay, step, amount });
+}
+
+// createPromise(2, 1500)
+// 	.then(({ position, delay }) => {
+// 		console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+// 	})
+// 	.catch(({ position, delay }) => {
+// 		console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+// 	});
