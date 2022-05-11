970f04e32f2ea98a6f48b3ea4f3bd3e543fb5fc6
Oleksii Borshch <sergeevi44@gmail.com>
2022-05-12 00:24:09 +0300

    task3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,15 +9,12 @@ refs.submitBtn.addEventListener("click", onSubmitBtnClick);
 
 function createPromise(position, delay) {
 	return new Promise((resolve, reject) => {
-		setInterval(() => {
+		setTimeout(() => {
 			const shouldResolve = Math.random() > 0.3;
 			if (shouldResolve) {
-				resolve(() => {
-					console.log("✅ Fulfilled promise");
-					return { position, delay };
-				}); // Fulfill
+				resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
 			} else {
-				reject(`❌ Rejected promise `); // Fulfill// Reject
+				reject(`❌ Rejected promise ${position} in ${delay}ms`);
 			}
 		}, delay);
 	});
@@ -25,30 +22,17 @@ function createPromise(position, delay) {
 
 function onSubmitBtnClick(evt) {
 	evt.preventDefault();
-	const delay = refs.form.delay.value;
+	let position = 0;
+	let delay = refs.form.delay.value;
 	const step = refs.form.step.value;
 	const amount = refs.form.amount.value;
-	for (let index = 1; index <= amount; index += 1) {
-		console.log(index);
-		let time = delay;
-		if (index != 1) {
-			time = delay + step * index;
+	for (let i = position; i < amount; i += 1) {
+		position += 1;
+		if (position != 1) {
+			delay = Number(delay) + Number(step);
 		}
-		createPromise(index, time)
-			.then(({ position, delay }) => {
-				console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-			})
-			.catch((error) => console.log(error));
+		createPromise(position, delay)
+			.then((result) => Notiflix.Notify.success(result))
+			.catch((error) => Notiflix.Notify.failure(error));
 	}
-	// createPromise(position, delay)
-	// 	.then((result) => console.log(result))
-	// 	.catch((err) => console.log(err));
 }
-
-// createPromise(2, 1500)
-// 	.then(({ position, delay }) => {
-// 		console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-// 	})
-// 	.catch(({ position, delay }) => {
-// 		console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-// 	});
